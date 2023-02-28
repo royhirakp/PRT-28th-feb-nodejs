669d2c2e3c079b93e5b2be95cb69a3cd155501c5
hirak-gitv2 <royhirakp@gmail.com>
2023-03-01 00:22:14 +0530

    env file delete

--- src/routes/ContactRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const RouteModel = require('../models/ComtactModel')
-
-router.get("/", async (req,res)=>{
-    res.json({
-        status:'wo route'
-    })
-})
